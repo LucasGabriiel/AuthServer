5e683050501fb96e64bbb1419471261ca9a4bfc9
Lucas Gabriel Tais <lucagabrieltais16@gmail.com>
2023-07-09 01:36:45 -0300

    Delete metod

--- src/main/kotlin/br/pucpr/authserver/drugstore/itens/ItemController.kt
+++ src/main/kotlin/br/pucpr/authserver/drugstore/itens/ItemController.kt
@@ -5,6 +5,7 @@ import br.pucpr.authserver.users.requests.UserRequest
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestBody
 
 class ItemController(val service: UsersService) {
@@ -13,4 +14,8 @@ class ItemController(val service: UsersService) {
         service.save(req)
             .toResponse()
             .let { ResponseEntity.status(HttpStatus.CREATED).body(it) }
+
+    fun delete(@PathVariable("id") id: Long): ResponseEntity<Void> =
+        if (service.delete(id)) ResponseEntity.ok().build()
+        else ResponseEntity.notFound().build()
 }
